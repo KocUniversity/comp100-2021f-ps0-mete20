12ae8c184fd1af1d4de12dc7210138680e9512df
Metehan Yaka <72347062+mete20@users.noreply.github.com>
2021-10-01 10:10:45 +0000

    I've added some visual things and welcome text

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import math
+print("*************************************")
+print("Welcome to my first assignment !!")
+print("*************************************")
 a = int(input("Enter number a: "))
 b = int(input("Enter number b: "))
 print("a**b =",a**b)
 print("log(a) is",math.log2(a))
 print("My student ID is 75852")
-
+print("*************************************")
